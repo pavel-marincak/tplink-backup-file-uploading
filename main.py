a866795ef5c95416a8955de8c954633bbbb0d8e3
pavel-marincak <61801828+pavel-marincak@users.noreply.github.com>
2021-11-15 09:25:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import tplink
+import os
 
 chromedriverpath = "chromedriver/chromedriver95"
 backupfilepath = "/home/mari/Documents/projects/tplink-backup-file-uploading/backupfile/EC230-G1V121022628368n.bin"
 adminip = "admin ip"
 ip = "default rtr ip"
 pwd = "rtr pwd"
+
+def isAlive(ip):
+    response = os.system("ping -w 1 " + ip + " >/dev/null 2>&1")
+    if response == 0:
+        return True
+    else:
+        return False
+
+tp = tplink.Tplink(chromedriverpath, backupfilepath, ip, pwd)
+tp.setUp(chromedriverpath)``
+
+while True:
+    if isAlive(ip):
+        try:
+            tp.setUp(chromedriverpath)
+            tp.setPwd(ip, pwd)
+        except:
+            tp.exit()
